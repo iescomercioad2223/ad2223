f28e475f2d46ab59f626f51114f22dcf7fd633c0
David <David@DESKTOP-46N0UF5.home>
2022-09-28 12:16:53 +0200

    Revisados ejemplos de Stax y añadidos nuevos

--- src/main/java/tema1/punto6/DStaxXMLStreamReaderApp.java
+++ src/main/java/tema1/punto6/DStaxXMLStreamReaderApp.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tema1.punto6;
-
  
 import javax.xml.stream.*;
 import java.io.*;
@@ -13,7 +7,6 @@ import java.io.*;
  * Ejemplo de lectura de datos con StAX a través del método: cursor API
  * Utilizando cursores
  */
-
 
 public class DStaxXMLStreamReaderApp {
 
